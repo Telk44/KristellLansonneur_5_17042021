7d28f2d6a14f47240a7aececaf5c87da929b66e5
Kristell L <klansonneur@hotmail.com>
2021-04-23 21:07:02 +0200

    index.js + html produit

--- front/js/index.js
+++ front/js/index.js
@@ -6,27 +6,29 @@ let request = new XMLHttpRequest();
  request.onreadystatechange = function() {
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
         let response = JSON.parse(this.responseText);  
-        for(let i of response){
-            let cameraPrice = new Intl.NumberFormat('fr-FE', { style: 'currency', currency: 'EUR',  minimumFractionDigits: 2 }).format(i.price/100);
-            document.querySelector("#listCamera").innerHTML += `<li class = "list-inline-item mb-4">   
-                                                                    <div class="card" style="width: 18rem;">
-                                                                        <img src="${i.imageUrl}" class="card-img-top" alt="..." width = "286px" height="190px">
-                                                                        <div class="card-body">
-                                                                            <h5 class="card-title">${i.name}</h5>
-                                                                            <p class="card-price">${cameraPrice}</p>
-                                                                            <a href="#" class="btn btn-primary">Découvrir</a>
-                                                                        </div>
-                                                                    </div>
-                                                                </li>`;
-                                                                
-        }    
+        displayProducts(response)
     }     
    
 };
 request.open("GET", "http://localhost:3000/api/cameras");
 request.send(); 
 
-
+const displayProducts = (response) =>{
+    for(let product of response){
+        let cameraPrice = new Intl.NumberFormat('fr-FE', { style: 'currency', currency: 'EUR',  minimumFractionDigits: 2 }).format(product.price/100);
+        let item = `<li class = "list-inline-item mb-4">   
+                        <div class="card" style="width: 18rem;">
+                            <img src="${product.imageUrl}" class="card-img-top" alt="..." width = "286px" height="190px">
+                            <div class="card-body">
+                                <h5 class="card-title">${product.name}</h5>
+                                <p class="card-price">${cameraPrice}</p>
+                                <a href="#" class="btn btn-primary">Découvrir</a>
+                            </div>
+                        </div>
+                     </li>`;
+    document.querySelector("#listCamera").innerHTML += item                                                                
+    }    
+}
 
 
 /* 
