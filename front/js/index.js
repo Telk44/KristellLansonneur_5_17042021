ce903db6c3f7bdfd9a3305cfdcdf69ebcb8404d0
Kristell L <klansonneur@hotmail.com>
2021-04-29 19:06:57 +0200

    page product.js finalisation cards - reste ajout panier

--- front/js/index.js
+++ front/js/index.js
@@ -1,21 +1,31 @@
-
-
 //Liens avec l'API
-
-let request = new XMLHttpRequest();
- request.onreadystatechange = function() {
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        let response = JSON.parse(this.responseText);  
-        displayProducts(response)
-        
-    }     
+const get = ()=>{
+  return new Promise((resolve,reject)=>{
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+       if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+           let response = JSON.parse(this.responseText);
+           resolve(response); 
+       }  
+       else if (this.status ===404) {
+           reject("erreur du serveur");
+       }
+   };
+   request.open("GET", "http://localhost:3000/api/camera");
+   request.send(); 
    
-};
-request.open("GET", "http://localhost:3000/api/cameras");
-request.send(); 
+  })  
+}
 
-const displayProducts = (response) =>{
-    for(let product of response){
+const displayProducts = async () =>{
+    let response = null;
+    try {
+        response = await get();
+    } catch (error) {
+        console.log('erreur');
+        
+    }
+       for(let product of response){
         let cameraPrice = new Intl.NumberFormat('fr-FE', { style: 'currency', currency: 'EUR',  minimumFractionDigits: 2 }).format(product.price/100);
         let item = `<li class = "list-inline-item mb-4">   
                         <div class="card" style="width: 18rem;">
@@ -30,6 +40,7 @@ const displayProducts = (response) =>{
     document.querySelector("#listCameras").innerHTML += item;                                                                
     }    
 }
+displayProducts();
  
 /* 
 class Article {
--- front/js/product.js
+++ front/js/product.js
@@ -1,32 +1,104 @@
+// Fonction récupération de l'ID de la camera dans l'URL
+
+function getId() {
+    // Récupération de la chaîne de requête dans l'URL 
+    let urlSearch = new URLSearchParams (window.location.search); 
+    // Récuperation de l'Id
+    let  productId = urlSearch.get("id"); 
+    return productId;     
+}
+  
+ //Fonction récupération de la caméra correspondant à l'Id
+
+ function getAssociatedCamera(cameras,idCamera) {
+    
+    let findCamera = cameras.find(cameras => cameras['_id'] === idCamera);
+    console.log('findCamera',findCamera);
+    return findCamera;    
+}
 
 
- /* async function getCameras(){ 
-    await fetch("http://localhost:3000/api/cameras")
-        .then(function(response) {
-        if (response.ok) {
-         let cameras = response.json();  
-         console.log(cameras);
-        //Récuperation de l'ID
-         let idCamera = getId();
-         console.log('idCamera',idCamera);
 
-        //Récupération de la camera associée
-        let camById= getAssociatedCamera(cameras,idCamera);
-        console.log('camById',camById);
+//Création de la structure html de la caméra choisie
 
-        //Création de la Card caméra
-        cardCamera(camById);                   
-        }
-    })
-     .then(function(value) {  
+function cardCamera(findCamera){
 
-    }) 
-    .catch(function(err) {
-        // Une erreur est survenue
-    });
-}  */
+    const productDescription= document.getElementById("productDescription");
+    let divParentParent = document.createElement("div");        
+    productDescription.appendChild(divParentParent);
+    divParentParent.classList.add("row", "mx-auto", "my-3", "w-75");
 
-// Fonction principale 
+    let divParent = document.createElement("div");
+    divParentParent.appendChild(divParent);
+    divParent.classList.add("card", "col", "m-auto", "p-5");
+
+    let imageCamera = document.createElement("img");
+    divParent.appendChild(imageCamera);
+    imageCamera.classList.add("card-image-top", "photo", "img-fluid");
+    imageCamera.src = findCamera.imageUrl;
+
+    let divCardBody = document.createElement("div");
+    divParent.appendChild(divCardBody);
+    divCardBody.classList.add("card-body", "text-center", "px-0", "d-flex", "flex-column", "justify-content-between");
+
+
+    // Création des enfants de la div CardBody
+
+    let titleCamera = document.createElement("h3");
+    divCardBody.appendChild(titleCamera);
+    titleCamera.classList.add("card-title");
+    titleCamera.textContent = findCamera.name;
+
+    let descriptionCamera = document.createElement("p");
+    divCardBody.appendChild(descriptionCamera);
+    descriptionCamera.classList.add("card-text", "text-justify");
+    descriptionCamera.textContent = findCamera.description;  
+    
+    //Appel fonction affichage du choix de l'objectif
+
+    chooseLense(divCardBody, findCamera);
+
+    // Création du prix  
+
+    let priceCamera = document.createElement("p");
+    let convertPrice = new Intl.NumberFormat('fr-FE', { style: 'currency', currency: 'EUR',  minimumFractionDigits: 2 }).format(findCamera.price/100);
+    divCardBody.appendChild(priceCamera);
+    priceCamera.classList.add("fw-bold", "align-self-center", "mb-5");
+    priceCamera.textContent = convertPrice;
+
+    // Création du bouton 
+    let buttonBuy = document.createElement("button");
+    divCardBody.appendChild(buttonBuy);
+    buttonBuy.classList.add("btn", "btn-primary", "align-self-center");
+    buttonBuy.textContent = "Ajouter au panier";       
+                                                                                                                                                                                                                                                        
+} 
+    //Ajout du choix des objectifs 
+
+function chooseLense(divCardBody, findCamera) {
+
+    let textChooseLens = document.createElement("p");
+    divCardBody.appendChild(textChooseLens);
+    textChooseLens.classList.add("text-left", "fw-bold","my-3");
+    textChooseLens.textContent = "Choisir l'objectif : ";
+
+    let choiceLens = document.createElement("select");
+    divCardBody.appendChild(choiceLens);
+    choiceLens.classList.add("col-md-4","align-self-center", "mb-5");
+    choiceLens.id = "list";
+
+    let linkLens = document.createElement("option");
+    choiceLens.appendChild(linkLens);
+    linkLens.textContent = "---"
+    
+    let numberLenses = findCamera.lenses;
+    for (let i = 0; i < numberLenses.length; i++) {
+         let optionLens = document.createElement("option");
+         choiceLens.appendChild(optionLens);
+         optionLens.textContent = findCamera.lenses[i];
+    }
+}
+    // Requête API
 
 const getCameraByID = async function(){  
     try{
@@ -42,9 +114,8 @@ const getCameraByID = async function(){
             //Récupération de la camera associée
            let camById= getAssociatedCamera(cameras,idCamera);
            console.log('camById',camById);
-           //Création de la Card caméra
+           //Appel fonction création de la Card caméra
            cardCamera(camById); 
-
 
         } else {
             console.error('retour du serveur: ', response.status)
@@ -53,93 +124,6 @@ const getCameraByID = async function(){
         console.log(e)
     }
 }
+   // Appel de la requête
 
-// Récupération de l'ID de la camera dans l'URL
-
-function getId() {
-    // Récupération de la chaîne de requête dans l'URL 
-    let urlSearch = new URLSearchParams (window.location.search); 
-    // Récuperation de l'Id
-    let  productId = urlSearch.get("id"); 
-    //console.log('productId',productId)
-    return productId;     
-}
-  
- //Récupération de la caméra correspondant à l'Id
-
-function getAssociatedCamera(cameras,idCamera) {
-    console.log('cameras2',cameras);
-    console.log('idCamera2',idCamera);
-    let findCamera = cameras.find(cameras => cameras['_id'] === idCamera);
-    console.log('findCamera',findCamera);
-    return findCamera;
-    
-}
-
-//création de la structure html de la caméra choisie
-
-function cardCamera(findCamera){
-
-        const productDescription= document.getElementById("productDescription");
-        let divParentParent = document.createElement("div");        
-        productDescription.appendChild(divParentParent);
-        divParentParent.classList.add("row", "mx-auto", "my-3", "w-75");
-    
-        let divParent = document.createElement("div");
-        divParentParent.appendChild(divParent);
-        divParent.classList.add("card", "col", "m-auto", "p-5");
-    
-        let imageCamera = document.createElement("img");
-        divParent.appendChild(imageCamera);
-        imageCamera.classList.add("card-image-top", "photo", "img-fluid");
-        imageCamera.src = findCamera.imageUrl;
-    
-        let divCardBody = document.createElement("div");
-        divParent.appendChild(divCardBody);
-        divCardBody.classList.add("card-body", "text-center", "px-0", "d-flex", "flex-column", "justify-content-between");
-    
-    
-        // Création des éléments enfants de la div CardBody
-        let titleCamera = document.createElement("h3");
-        divCardBody.appendChild(titleCamera);
-        titleCamera.classList.add("card-title");
-        titleCamera.textContent = findCamera.name;
-    
-        let descriptionCamera = document.createElement("p");
-        divCardBody.appendChild(descriptionCamera);
-        descriptionCamera.classList.add("description-product", "text-justify");
-        descriptionCamera.textContent = findCamera.description;
-    
-        chooseLense(divCardBody, FindCamera);
-    
-        // Création d'une div englobant prix et bouton
-        let divPrice = document.createElement("div");
-        divCardBody.appendChild(divPrice);
-        divPrice.classList.add("d-flex", "flex-md-row", "flex-column", "justify-content-between");
-    
-        // Création du prix
-        let priceCamera = document.createElement("p");
-        divLinkPrice.appendChild(priceCamera);
-        priceCamera.classList.add("price-product", "font-weight-bold");
-        priceCamera.textContent = findcamera.price/100 + ' €';
-    
-        let linkProduct = document.createElement("a");
-        divLinkPrice.appendChild(linkProduct);
-
-        // Création du bouton 
-        let buyButton = document.createElement("button");
-        linkProduct.appendChild(buyButton);
-        buttonBuy.classList.add("btn", "btn-warning", "block-right");
-        // Ajout texte au bouton
-        buttonBuy.textContent = "Ajouter au panier";
-        console.log(idCamera);
-        getLensCamera(buyButton, idCamera);
-    }
-
-    //getCameras();
-    // Appel de la fonction principale
     getCameraByID();
-
-
-
-
